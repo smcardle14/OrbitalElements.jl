74f783212b365fb02a9b7b2a9cc4a4657e75efc7
Sean <seanmcardle14@gmail.com>
2022-07-28 19:12:09 -0500

    added rv2coe

--- src/OrbitalElements.jl
+++ src/OrbitalElements.jl
@@ -7,8 +7,8 @@ using StaticArrays
 
 Convert classical orbital elements 'coe' to Cartesian position and velocity 'rv'.
 
-Order classical orbital elements like so: [a, e, i, ω, Ω, ν]. 
-See Algorithm 9 from Vallado, D. "Fundamentals of Astrodynamics and Applications." 
+Input classical orbital elements in order: [a, e, i, ω, Ω, ν]. 
+See Algorithm 10 from Vallado, D. "Fundamentals of Astrodynamics and Applications." 
 4th Edition. 2013.
 
 See also [`rv2coe`](@ref)
@@ -40,13 +40,94 @@ function coe2rv(coe, μ=1)
     v = R*vpqw
 
     rv = vcat(r, v)
-
+    
     return rv
 end
 
+"""
+    rv2coe(rv, μ=1)
+
+Convert Cartesian position and velocity to classical orbital elements.
+
+
+Output classical orbital elements in order: [a, e, i, ω, Ω, ν]. 
+See Algorithm 9 from Vallado, D. "Fundamentals of Astrodynamics and Applications." 
+4th Edition. 2013.
+
+See also [`coe2rv`](@ref)
+
+"""
 function rv2coe(rv, μ=1)
-    coe=rv
+
+    x, y, z, u, v, w = rv
+
+    rvec = @SVector([x, y, z])
+    vvec = @SVector([u, v, w])
+    hvec = cross_local(rvec, vvec)
+    nvec = cross_local(@SVector([0,0,1]), hvec)
+
+    r = sqrt(sum(rvec.^2))
+    v = sqrt(sum(vvec.^2))
+    h = sqrt(sum(hvec.^2))
+    n = sqrt(sum(nvec.^2))
+
+    rdotv = sum(rvec.*vvec)
+    evec = ((v^2-μ/r)*rvec - rdotv*vvec)/μ
+
+    e = sqrt(sum(evec.^2))
+
+    ξ = 0.5*v^2 - μ/r
+
+    if (abs(e - 1.0) > 1.0e-16)
+        a = -0.5*μ/ξ
+        p = a*(1-e^2)
+    else
+        p = h^2/μ
+        a = Inf
+    end
+
+    i = acos(hvec[3]/h)
+
+    Ω = acos(nvec[1]/n)
+    if nvec[2] < 0.0; Ω = 2.0*π - Ω; end
+
+    ndote = sum(nvec.*evec)
+    ω = acos(ndote/n/e)
+    if evec[3] < 0.0; ω = 2.0*π - ω; end
+
+    edotr = sum(evec.*rvec)
+    ν = acos(edotr/e/r)
+    if rdotv < 0.0; ν = 2.0*π - ν; end
+
+    # Special cases
+    circular = abs(e) < 1.0e-16
+    equatorial = abs(i) < 1.0e-16
+    if !circular && equatorial # ν is now true longitude of periapsis
+        ω = 0.0
+        ν = acos(evec[1]/e)
+        if evec[2] < 0.0; ν = 2.0*π - ν; end
+    elseif circular && !equatorial # ν is now argument of latitude
+        Ω = 0.0
+        ndotr = sum(nvec.*rvec)
+        ν = acos(ndotr/n/r)
+        if rvec[3] < 0.0; ν = 2.0*π - ν; end
+    elseif circular && equatorial # ν is now true longitude
+        ω = 0.0
+        Ω = 0.0
+        ν = acos(r[1]/r)
+        if rvec[2] < 0; ν = 2.0*π - ν; end
+    end
+
+    coe = @SVector([a, e, i, ω, Ω, ν])
+
     return coe
+end
+
+function cross_local(a, b)
+    return @SVector([a[2]*b[3]-a[3]*b[2], 
+                     a[3]*b[1]-a[1]*b[3], 
+                     a[1]*b[2]-a[2]*b[1]])
+
 end
 
 function rot1(θ)
@@ -64,5 +145,6 @@ function rot3(θ)
 end
 
 export coe2rv
+export rv2coe
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,27 @@
 using OrbitalElements
 using Test
+using BenchmarkTools
+
+deg2rad = π/180.0
+μ = 398600.4418
+
+# Vallado test case (Example 2-6)
+p = 11067.790
+e = 0.83285
+coe = [p/(1-e^2), e, 87.87*deg2rad, 53.38*deg2rad, 227.89*deg2rad, 92.335*deg2rad]
+rv = [6525.36812098609, 6861.531834896053, 6449.11861416016,
+      4.902278646418963, 5.533139568361491, -1.975710099535108]
 
 @testset "OrbitalElements.jl" begin
 
     @test coe2rv([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]) == [1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+    @test isapprox(coe2rv(coe, μ), rv, rtol=1e-15*μ)
 
-    # Vallado test case (Example 2-6)
-    deg2rad = π/180.0
-    p = 11067.790
-    e = 0.83285
-    a = p/(1-e^2)
-    i = 87.87*deg2rad
-    ω = 53.38*deg2rad
-    Ω = 227.89*deg2rad
-    ν = 92.335*deg2rad
-    μ = 398600.4418
-    # More precision than Vallado's result
-    @test isapprox(coe2rv([a, e, i, ω, Ω, ν], μ), 
-        [6525.36812098609, 6861.531834896053, 6449.11861416016,
-         4.902278646418963, 5.533139568361491, -1.975710099535108], 
-        rtol=1e-15)
+    @test rv2coe([1.0, 0.0, 0.0, 0.0, 1.0, 0.0]) == [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    @test isapprox(rv2coe(rv, μ), coe, rtol=1e-15*μ)
+
+    println("benchmarking coe2rv...")
+    @btime coe2rv($coe, $μ)
+    println("benchmarking rv2coe...")
+    @btime rv2coe($rv, $μ)
 end
